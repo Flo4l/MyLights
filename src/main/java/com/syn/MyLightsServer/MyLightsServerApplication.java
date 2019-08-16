95a8977bd30cdc4c0114712de4e91653601100f1
Florian Albers <Florian.Albers@adesso.de>
2019-08-16 14:15:28 +0200

    Initial Commit

---
+++ src/main/java/com/syn/MyLightsServer/MyLightsServerApplication.java
@@ -0,0 +1,13 @@
+package com.syn.MyLightsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLightsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLightsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syn/MyLightsServer/MyLightsServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syn.MyLightsServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyLightsServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
