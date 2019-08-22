a73b1b810b5f384fa9e69083c315b69c51513af9
Florian Albers <Florian.Albers@adesso.de>
2019-08-22 14:53:31 +0200

    Added functionality for registering MCUs on their startup.

---
+++ src/main/java/com/syn/MyLightsServer/stripe/exceptions/InvalidStripeIpException.java
@@ -0,0 +1,8 @@
+package com.syn.MyLightsServer.stripe.exceptions;
+
+public class InvalidStripeIpException extends RuntimeException {
+
+	public InvalidStripeIpException() {
+		super();
+	}
+}
---
+++ src/main/java/com/syn/MyLightsServer/stripe/exceptions/InvalidStripeMacException.java
@@ -0,0 +1,8 @@
+package com.syn.MyLightsServer.stripe.exceptions;
+
+public class InvalidStripeMacException extends RuntimeException {
+
+	public InvalidStripeMacException() {
+		super();
+	}
+}
