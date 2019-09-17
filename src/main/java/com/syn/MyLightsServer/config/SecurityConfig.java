54583dc9f2a6a3bf6e63c5150650856140f536b8
Florian Albers <Florian.Albers@adesso.de>
2019-09-17 10:17:33 +0200

    Set up forced login

--- src/main/java/com/syn/MyLightsServer/config/SecurityConfig.java
+++ src/main/java/com/syn/MyLightsServer/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.syn.MyLightsServer.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +9,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+	@Autowired
+	public SecurityConfig( DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)
 			throws Exception {
-		auth.inMemoryAuthentication().withUser("user")
+		auth.inMemoryAuthentication().withUser("username")
 				.password("password").roles("USER");
-	}
+}
 
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/**")
-				.permitAll()
+                .authenticated()
 				.and()
 				.logout()
 				.logoutUrl("/logout")
@@ -35,8 +53,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.headers().frameOptions().disable();
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery)
+				.dataSource(dataSource).passwordEncoder(passwordEncoder());
+	}
+
 	@Bean
-	protected PasswordEncoder passwordEncoder() {
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(dataSource);
+		return tokenRepository;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 }
---
+++ src/main/java/com/syn/MyLightsServer/user/persistence/Role.java
@@ -0,0 +1,38 @@
+package com.syn.MyLightsServer.user.persistence;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleID;
+
+    private String role;
+
+    public Role() {
+        this("");
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
